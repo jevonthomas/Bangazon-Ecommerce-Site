fc71e3f4e0376d5be0dc6ce65bbaa42113f9fd7d
jevonthomas <jevon.g.thomas@gmail.com>
2017-10-21 07:45:12 -0500

    set up routing for viewing all products when /products is put into the browser

--- controllers/productsCtrl.js
+++ controllers/productsCtrl.js
@@ -1,13 +1,13 @@
 'use strict'
 
 module.exports.getProducts = (req, res, next) => {
-  console.log("Product");
-  // const { Products } = req.app.get('models');
-  // Products.findAll()
-  // .then( (products) => {
-  //   res.render('product', {products});
-  // })
-  // .catch( (err) => {
-  //   next(err); 
-  // });
+  const { Products } = req.app.get('models');
+  Products.findAll()
+  .then( (products) => {
+    console.log("Product", products);
+    // res.render('product', {products});
+  })
+  .catch( (err) => {
+    next(err); 
+  });
 };
